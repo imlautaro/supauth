970db2c961c8d4bd9c634e470fe480887f58d9a9
Lautaro Pereyra <hi@imlautaro.com>
2022-12-29 10:36:10 -0300

    fix design issue in forms

--- src/layouts/supauth.vue
+++ src/layouts/supauth.vue
@@ -1,5 +1,9 @@
 <template>
-	<Stack class="max-w-sm min-h-screen mx-auto px-6 py-12" items="center">
+	<Stack
+		class="max-w-sm min-h-screen mx-auto px-6 py-12"
+		justify="center"
+		vertical
+	>
 		<slot />
 	</Stack>
 </template>
